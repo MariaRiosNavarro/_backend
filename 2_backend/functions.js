8d8063598d01543ab39b0ca21627153b1f31d4e2
SuperCoder <propscode254@gmail.com>
2023-11-23 13:23:58 +0100

    add functions

---
+++ 2_backend/functions.js
@@ -0,0 +1,45 @@
+
+
+export const  firstOfArray= () => {
+    return (  );
+}
+
+export const  allWithoutLast= () => {
+    return (  );
+}
+
+export const  lastOfArray= () => {
+    return (  );
+}
+
+export const  allWithoutFirst= () => {
+    return (  );
+}
+
+export const  removeValueOfArray= (value) => {
+    return (  );
+}
+
+export const  removeDuplicate= () => {
+    return (  );
+}
+
+export const  sumeAllArrayNumbers= () => {
+    return (  );
+}
+ 
+export const  randomNumberBetween= (a,b) => {
+    return (  );
+}
+
+export const  firstLetterUpercase= (string) => {
+    return (  );
+}
+
+export const  allLetterUpercase= (string) => {
+    return (  );
+}
+
+export const  sameLastLetter= (stringA, letter) => {
+    return (  );
+}
