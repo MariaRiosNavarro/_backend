a9065a3e806b0ec352f10aba679f33a7b19130c6
SuperCoder <propscode254@gmail.com>
2023-11-24 10:41:24 +0100

    add

--- _u_24_11/index.js
+++ _u_24_11/index.js
@@ -1,5 +1,6 @@
 //Einführung Asynchron mit Callbacks
 
+import { log } from "console";
 import fs from "fs";
 //In Node.js steht "fs" für "File System",
 //was auf Deutsch "Dateisystem" bedeutet.
@@ -24,12 +25,38 @@ import fs from "fs";
 
 //fs.writeFile("PFAD", "WAS MUSS IN DER DATEI DRIN", CALLBACK)
 
-// fs.writeFile("./geschichte", "Da ist ein String", () => {
-//   console.log("Meine Datei ist geschrieben");
-// });
+fs.writeFile("./geschichte", "Da ist ein String", () => {
+  console.log("Meine Datei ist geschrieben");
+});
 
 fs.writeFile("./new", "andere", () => {
   console.log("Meine Datei new ist geschrieben");
 });
 
-fs.readFile("./geschichte");
+//Datei auslesen, 2 versionen die ergebnisse als string zu geben
+
+// VersionA
+
+fs.readFile("./geschichte", (err, data) => {
+  if (err) {
+    console.log("Das ist kaput", err);
+    return;
+  }
+  console.log(String(data)); // Ohne String kommt das: <Buffer 44 61 20 69 73 74 20 65 69 6e 20 53 74 72 69 6e 67>, wir mussen unwandeln
+});
+
+// VersionB
+
+// fs.readFile("./geschichte", { encoding: "utf8" }, (err, data) => {
+//   if (err) {
+//     console.log("Das ist kaput", err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+console.log("BIN UNTEN IN DEN FILE");
+
+// https://nodejs.org/dist/latest-v20.x/docs/api/fs.html#file-system-flags
+
+// fs.writeFile('./geschichte', 'new new', {flag:''})
