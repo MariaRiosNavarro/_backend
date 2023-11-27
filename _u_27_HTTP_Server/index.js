72a2161ebfca839031149a2f0666837d309e54a6
SuperCoder <propscode254@gmail.com>
2023-11-27 10:58:18 +0100

    add

--- _u_27_HTTP_Server/index.js
+++ _u_27_HTTP_Server/index.js
@@ -1,7 +1,25 @@
 import http from "http";
 
 const requestHandler = (req, res) => {
-  res.end("Hello Back");
+  console.log("URL", req.url);
+  console.log("Methode", req.method);
+  // Methoden:
+  // GET - gibt mir die resouerce
+  // POST - erstelle neue resource
+  // PUT - aktuelisiere existierende resources
+  // DELETE - losche resources
+
+  //Header configuracion, Body- Daten oder Payload
+
+  if (req.url === "/") {
+    res.end("MainPage");
+    return;
+  } else if (req.url === "/about") {
+    res.end("Das sind wir");
+  } else {
+    res.writeHead(404);
+    res.end("Page not Found");
+  }
 };
 
 const server = http.createServer(requestHandler);
