902663d63aadc949261491f4c6fba433634fa5bc
SuperCoder <propscode254@gmail.com>
2023-11-28 13:16:01 +0100

    done

--- 8_backend/index.js
+++ 8_backend/index.js
@@ -23,3 +23,20 @@ doubleNumberAsync(0)
 doubleNumberAsync(85473)
   .then((resolveResult) => console.log("The Double is: ", resolveResult))
   .catch((resolveReject) => console.log("Double of 0 is: ", resolveReject));
+
+//   2.1
+
+const ramdomNumberBiggerTo6 = () => {
+  return new Promise((resolve, reject) => {
+    const randomNumber = Math.round(Math.random() * 10) + 1;
+    if (randomNumber > 6) {
+      resolve("number was : " + randomNumber);
+    } else {
+      reject("number was : " + randomNumber);
+    }
+  });
+};
+
+ramdomNumberBiggerTo6()
+  .then((resolveResult) => console.log("Resolve Result is:", resolveResult))
+  .catch((rejectResult) => console.log("Reject Result is: ", rejectResult));
