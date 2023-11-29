a0cbc4a9a6b59cd0b1c403935bdae393afd6a69d
SuperCoder <propscode254@gmail.com>
2023-11-29 12:14:44 +0100

    works

--- _superreise/app.js
+++ _superreise/app.js
@@ -1,7 +1,25 @@
 import express from "express";
+import { allPlanes, savePlane, setup } from "./utils/filestorage.js";
 
 const PORT = 9999;
 const app = express();
+
+setup();
+
+app.use(express.json());
+
+//wir haben 2 ruten gemacht : eine lies andere hinzufugt. Wir werden in fs eine funktion machenund hier benutzen um ie sachen zu speichern
+
+app.get("/api/planes", (req, res) => {
+  allPlanes().then((planes) => res.json(planes));
+});
+
+app.post("/api/planes", (req, res) => {
+  //neue Flugzeug speichern
+  const plane = req.body;
+  savePlane(plane);
+  res.end();
+});
 
 app.listen(PORT, () => {
   console.log("Port is:" + PORT);
--- _superreise/utils/filestorage.js
+++ _superreise/utils/filestorage.js
@@ -1,1 +1,27 @@
 import fs from "fs/promises";
+
+//herstellt json
+
+export const setup = () => {
+  fs.access("./planes.json")
+    .then((data) => console.log(data))
+    .catch((err) => fs.writeFile("./planes.json", "[]"));
+};
+
+//liest daten
+
+export const allPlanes = () => {
+  return fs
+    .readFile("./planes.json", { encoding: "utf8" })
+    .then((data) => JSON.parse(data));
+};
+
+export const savePlane = (plane) => {
+  allPlanes()
+    .then((data) => {
+      data.push(plane);
+      console.log(data);
+      return data;
+    })
+    .then((array) => fs.writeFile("./planes.json", JSON.stringify(array)));
+};
